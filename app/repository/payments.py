c71996310db909ce364bea8718ff6e35f165c4cc
Дмитрий Курчаев <truggjhuhff@gmail.com>
2026-06-28 01:52:42 +0300

    Revert "для реверта"
    This reverts commit 0affba4a4493a2d5ccf5bba93a9b0b132759d680.
    Давай по новой, Миша

--- app/repository/payments.py
+++ app/repository/payments.py
@@ -4,7 +4,6 @@ from app.models import Transaction
 
 
 
-
 PAYMENTS = [{
     "id": "28282",
     "amount": 200,
@@ -16,19 +15,4 @@ PAYMENTS = [{
     "webhook_url": "example.com",
     "created_at": "2024-01-01"
 }]
-
-def add_payment(body:):
-    new_payment = {
-        "id": int(uuid.uuid4()),
-        "amount": body.amount,
-        "currency": body.currency,
-        "description": body.description,
-        "metadata": body.meta,
-        "status": "pending",
-        "idempotency_key": idempotency_key,
-        "webhook_url": body.webhook_url,
-        "created_at": datetime.now(timezone.utc)
-}
-PAYMENTS.append(new_payment)
-PROCESSED[idempotency_key] = new_payment
 
