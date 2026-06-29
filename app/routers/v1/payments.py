0662f09aabb9459936057b92a7d4b810971e44bc
Дмитрий Курчаев <truggjhuhff@gmail.com>
2026-06-29 16:33:19 +0300

    Добавил зависимость для подключения к БД

--- app/routers/v1/payments.py
+++ app/routers/v1/payments.py
@@ -1,16 +1,17 @@
-from fastapi import Response, Header
+from fastapi import Response, Header, Depends
 from fastapi import APIRouter
+from sqlalchemy.orm.session import Session
+
 from app.schemas import PaymentCreateResponse, PaymentCreateRequest
 import app.services.payments as payment_service
-from datetime import datetime, timezone
-import uuid
+from dependency import db_connection
 router = APIRouter()
 @router.get("/{payment_id}")
-def get_payment(payment_id:str):
-    return payment_service.get_payment(payment_id=payment_id)
+def get_payment(payment_id:str, db:Session=Depends(db_connection)):
+    return payment_service.get_payment(payment_id=payment_id, db=db)
 
 
 
 @router.post("/")
-def create_payment(body:PaymentCreateRequest, idempotency_key:str=Header(..., max_length=50)):
-    return payment_service.create_payment(body=body,idempotency_key=idempotency_key)
+def create_payment(body:PaymentCreateRequest, idempotency_key:str=Header(..., max_length=50), db:Session=Depends(db_connection)):
+    return payment_service.create_payment(body=body,idempotency_key=idempotency_key, db=db)
--- app/services/payments.py
+++ app/services/payments.py
@@ -1,20 +1,20 @@
 from fastapi import Response, Header, HTTPException
+from sqlalchemy.orm.session import Session
+
 from app.schemas import PaymentCreateResponse, PaymentCreateRequest
 from datetime import datetime, timezone
 from app.repository import payments as payments_repository
 from app.database import SessionLocal
 
-def get_payment(payment_id:str):
-    db = SessionLocal()
+def get_payment(payment_id:str, db:Session):
     data = payments_repository.get_payment(db=db, payment_id=payment_id)
     if data:
         return data
     else:
         raise HTTPException(status_code=400, detail=f"There are no transactions with id {payment_id}")
 
-def create_payment(body:PaymentCreateRequest, idempotency_key:str=Header(..., max_length=50)):
-    db = SessionLocal()
-    if info:=payments_repository.does_payment_exist(db=db, idempotency_key=idempotency_key):
+def create_payment(db:Session, body:PaymentCreateRequest, idempotency_key:str=Header(..., max_length=50)):
+    if info:=payments_repository.does_payment_exist(idempotency_key=idempotency_key, db=db):
         return {"message": "Payment already exists", "info": info}
     else:
         data = payments_repository.create_payment(db=db, body=body, idempotency_key=idempotency_key)
